6530d49d0da399dc1e102f268026c7e81427b97d
Josh Caskie <57299645+joshcaskie@users.noreply.github.com>
2021-11-03 16:07:52 -0400

    Digital Ocean Deployed and Working #45
    Change the WebSocket path in Messenger.jsx and add the env file lines to the Dockerfile after pulling!

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -3,6 +3,7 @@ import React, {useState, useContext, useEffect} from 'react';
 const io = require("socket.io-client");
 
 let endPoint = "http://localhost:5000"; // WebSocket conns made here now!
+// let endPoint = "http://137.184.97.255:5000"; // (For Deployment change this line)
 let socket = io.connect(`${endPoint}`);  // https://medium.com/analytics-vidhya/simple-chat-app-with-react-flask-b2ae72404fcb
 
 const Messenger = () => {
