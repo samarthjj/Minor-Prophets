d2b483740959a3c01fea1b2cbc6f4f6303ce5b59
HarrisonHutton <harrison.hutton6@gmail.com>
2021-11-03 23:05:23 -0400

    refactored grabTrackArt and grabAlbumArt #46
    These two functions now take in IDs rather than a search term. They still return the same image url as before.

--- flask/spotify_utils.py
+++ flask/spotify_utils.py
@@ -31,6 +31,7 @@ def grabTrackYear(track_name, artist_name, type='track', limit=30):
     release_year = release_date[0:4]
     return release_year
 
+
 #Duplicate code here
 def grabAlbumYear(album_name, artist_name, type='album', limit=1):
     results = sp.search(q=album_name, type=type, limit=limit)
@@ -50,31 +51,26 @@ def grabAlbumYear(album_name, artist_name, type='album', limit=1):
     release_year = release_date[0:4]
     return release_year
 
-#Duplicate code here
-def grabTrackArt(track_name, artist_name, type='track', limit=30):
-    results = sp.search(q=track_name, type=type, limit=limit)
-    # Largest image pixel-wise from Spotify
-    track_art = ""
-    goal = artist_name.upper()
-    for choice in results["tracks"]["items"]:
-        found_artist_name = choice["artists"][0]["name"]
-        if found_artist_name.upper() == goal:
-            track_art = choice["album"]["images"][0]["url"]
 
-    return track_art
+def grabTrackArt(track_id):
+    """
+    Given a track ID, return a temporary URl to the 640x640 image associated with the track. The URL expires in
+    under 24 hours from the time of conception.
+    """
+    track_data = sp.track(track_id)
+    art = track_data["album"]["images"][0]["url"]
+    return art
 
-#Duplicate code here
-def grabAlbumArt(album_name, artist_name, type='album', limit=1):
-    results = sp.search(q=album_name, type=type, limit=limit)
-    # Largest image pixel-wise from Spotify
-    album_art = ""
-    goal = artist_name.upper()
-    for choice in results["albums"]["items"]:
-        found_artist_name = choice["artists"][0]["name"]
-        if found_artist_name.upper() == goal:
-            album_art = choice["images"][0]["url"]
 
-    return album_art
+def grabAlbumArt(album_id):
+    """
+    Given an album ID, return a temporary URL to the 640x640 image associated with the album. The URL expires in
+    under 24 hours from the time of conception.
+    """
+    album_data = sp.album(album_id)
+    art = album_data["images"][0]["url"]
+    return art
+
 
 def pickOutID(playlist_url):
     """
@@ -83,24 +79,27 @@ def pickOutID(playlist_url):
     """
     return playlist_url[34:56]
 
+
 def listPlaylistTrackIDs(playlist_url):
     """
     Given a playlist url, this function will return a list of track IDs from that playlist that can later
     be used to grab whole track information.
     """
-    playlist_ID = pickOutID(playlist_url)
-    playlist_data = sp.playlist(playlist_ID)
-    track_IDs = [song["track"]["id"] for song in playlist_data["tracks"]["items"]]
-    return track_IDs
+    playlist_id = pickOutID(playlist_url)
+    playlist_data = sp.playlist(playlist_id)
+    track_ids = [song["track"]["id"] for song in playlist_data["tracks"]["items"]]
+    return track_ids
 
-def grabTrackName(track_ID):
+
+def grabTrackName(track_id):
     """
     Given a track ID, return the track name associated with it.
     """
-    return sp.track(track_ID)["name"]
+    return sp.track(track_id)["name"]
 
-def grabTrackPopularity(track_ID):
+
+def grabTrackPopularity(track_id):
     """
     Given a track ID, return the popularity score associated with it as an integer between 0-100 inclusive.
     """
-    return sp.track(track_ID)["popularity"]
+    return sp.track(track_id)["popularity"]
--- flask/testing_spotify_utils.py
+++ flask/testing_spotify_utils.py
@@ -6,20 +6,7 @@ Running these function calls to test correctness.
 
 from spotify_utils import *
 
-tracks_test_these = [
-    ("call me maybe", "carly rae jepsen", 2012),
-    ("last call", "plini", 2020),
-    ("willow", "taylor swift", 2020)
-]
-
-albums_test_these = [
-    ("call me maybe", "carly rae jepsen", 2012),
-    ("impulse voices", "plini", 2020),
-    ("evermore", "taylor swift", 2020)
-]
-
 testingPlaylist = "https://open.spotify.com/playlist/23ix3PPVfL1JGc4Zj1GpcL?si=f9207fe35cdb4a56"
-
 
 # Test 1: Receive the correct release year for a given TRACK and artist
 """ These are passing
@@ -37,20 +24,17 @@ for index in range(len(albums_test_these)):
     print(f"Expected: {albums_test_these[index][2]} ----- Returned: {grabAlbumYear(album, artist)}")
 """
 
-# Test 3: Receive the correct album art for a given TRACK and artist
-""" These are passing
-for index in range(len(tracks_test_these)):
-    track = tracks_test_these[index][0]
-    artist = tracks_test_these[index][1]
-    print(f"IMAGE URL: {grabTrackArt(track, artist)}")
+# Test 3: Receive the correct album art for a given TRACK
+""" This is passing
+single_track = listPlaylistTrackIDs(testingPlaylist)[0]
+print(grabTrackArt(single_track))
 """
 
-# Test 4: Receive the correct album art for a given ALBUM and artist
-""" These are passing
-for index in range(len(albums_test_these)):
-    album = albums_test_these[index][0]
-    artist = albums_test_these[index][1]
-    print(f"IMAGE URL: {grabAlbumArt(album, artist)}")
+
+# Test 4: Receive the correct album art for a given ALBUM
+""" This is passing
+single_album = "0zFqkobYDLzAAHbK2lgc68"
+print(grabAlbumArt(single_album))
 """
 
 # Test 5: Grabbing correct playlist ID from a playlist URL
