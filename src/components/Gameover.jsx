157347d9993e6ba1c7a4e0b8fafd034af171d2f4
Josh Caskie <57299645+joshcaskie@users.noreply.github.com>
2021-11-10 02:06:09 -0500

    Gameover screen shows game state #52

--- src/components/Gameover.jsx
+++ src/components/Gameover.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useContext} from "react";
 import {Link, useParams} from "react-router-dom";
 import { SocketContext} from '../socket';
+import {default as axios} from "axios";
 
 
 const Gameover = () => {
@@ -8,6 +9,36 @@ const Gameover = () => {
     const socket = useContext(SocketContext);
 
     const { room_code } = useParams();
+
+    function get_score() {
+        axios.get('/api/get_scores', {
+            params: {
+                roomcode: room_code
+            }
+        })
+            .then(function (response) {
+                // console.log(response.data.User)
+                // console.log(response.data.Score)
+                let user = response.data.User
+                let score = response.data.Score
+                // let output = ""
+                let tableUsers = ["p1","p2","p3","p4","p5"]
+                let tableScores = ["p1score","p2score","p3score","p4score","p5score"]
+                for (let i=0; i<user.length && i < 5; i++) {
+                    document.getElementById(tableUsers[i]).innerHTML = user[i]
+                    document.getElementById(tableScores[i]).innerHTML = score[i]
+                    // output += user[i] + ": " + score[i] + " "
+                }
+                // document.getElementById("scores").innerHTML = output
+
+                // for (const [key, value] of Object.entries(response.data)) {
+                //     console.log(key);
+                //     console.log(value);
+                // }
+        })
+    }
+
+    get_score();
 
     useEffect(() => {
 
@@ -46,41 +77,41 @@ const Gameover = () => {
 
             {/*Winner*/}
             <div className="row mb-3">
-                <h1 className="text text-light">Winner: Harrison</h1>
+                <h1 className="text text-light">Game Over!</h1>
             </div>
 
             {/*Scoreboard*/}
             <div className="row mb-3">
                 <table className="table table-striped table-success">
-                    <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Points</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Harrison</td>
-                        <td>145</td>
-                    </tr>
-                    <tr>
-                        <td>Sam</td>
-                        <td>135</td>
-                    </tr>
-                    <tr>
-                        <td>Maeve</td>
-                        <td>120</td>
-                    </tr>
-                    <tr>
-                        <td>Josh</td>
-                        <td>100</td>
-                    </tr>
-                    <tr>
-                        <td>Joe</td>
-                        <td>95</td>
-                    </tr>
-                    </tbody>
-                </table>
+                        <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Points</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td id = "p1"> </td>
+                            <td id = "p1score"> </td>
+                        </tr>
+                        <tr>
+                            <td id = "p2"> </td>
+                            <td id = "p2score"> </td>
+                        </tr>
+                        <tr>
+                            <td id = "p3"> </td>
+                            <td id = "p3score"> </td>
+                        </tr>
+                        <tr>
+                            <td id = "p4"> </td>
+                            <td id = "p4score"> </td>
+                        </tr>
+                        <tr>
+                            <td id = "p5"> </td>
+                            <td id = "p5score"> </td>
+                        </tr>
+                        </tbody>
+                    </table>
             </div>
 
         </div>
